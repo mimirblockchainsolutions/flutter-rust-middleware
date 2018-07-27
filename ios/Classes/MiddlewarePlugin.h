5a27b88834eef104d6cf453fc745fa72402457ec
nickfierro <nickfierro@gmail.com>
2018-07-27 13:15:46 -0400

    attempting to make plugin, ios doesn't like the c handles for the rust code in midldlewareplugin.h

--- ios/Classes/MiddlewarePlugin.h
+++ ios/Classes/MiddlewarePlugin.h
@@ -1,4 +1,8 @@
 #import <Flutter/Flutter.h>
+#include <stdint.h>
+
+const char* request_function(const char* to);
+void function_free(char *);
 
 @interface MiddlewarePlugin : NSObject<FlutterPlugin>
 @end
