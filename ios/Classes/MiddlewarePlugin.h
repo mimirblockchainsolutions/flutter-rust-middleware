9ea589c7477f9200ac37eed231c0ce10cc8300b3
nickfierro <nickfierro@gmail.com>
2018-08-08 10:46:41 -0400

    fresh start on ios plugin

--- ios/Classes/MiddlewarePlugin.h
+++ ios/Classes/MiddlewarePlugin.h
@@ -1,8 +1,4 @@
 #import <Flutter/Flutter.h>
-#include <stdint.h>
-
-const char* request_function(const char* to);
-void function_free(char *);
 
 @interface MiddlewarePlugin : NSObject<FlutterPlugin>
 @end
--- ios/Classes/SwiftMiddlewarePlugin.swift
+++ ios/Classes/SwiftMiddlewarePlugin.swift
@@ -1,6 +1,6 @@
 import Flutter
 import UIKit
-
+    
 public class SwiftMiddlewarePlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "middleware", binaryMessenger: registrar.messenger())
@@ -8,27 +8,7 @@ public class SwiftMiddlewarePlugin: NSObject, FlutterPlugin {
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
-  private func middleWare(args: String, result: FlutterResult) {
-    let middleWare = MiddleWare();
-    let reslt = middleWare.call(to: args);
-    result(reslt);
-  }
-
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    if ("middleWare" == call.method) {
-        let jsonResult = call.arguments as! NSArray;
-        let array = jsonResult as? [Any];
-        let argsObject = array?.first;
-        let aaargs = argsObject as? [String: Any];
-        let jsonData = try! JSONSerialization.data(withJSONObject: aaargs!, options: .prettyPrinted);
-        let jsonString = NSString(data: jsonData, encoding: String.Encoding.utf8.rawValue)! as String;
-        print(jsonString);
-        
-        self.middleWare(args: jsonString, result: result);
-    } else {
-        result(FlutterMethodNotImplemented);
-    };
-    
     result("iOS " + UIDevice.current.systemVersion)
   }
 }
